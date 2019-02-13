d1029b9ae7b414451e0d782a57eb342ec7f51934
hkhaliq <44483097+hkhaliq@users.noreply.github.com>
2019-02-13 16:26:26 -0500

    Update practice1.cpp

--- Week2/practice1.cpp
+++ Week2/practice1.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <math.h>
 
+using namespace std;
+
+int main()
+{
+    int input = 0;
+    
+    while(1){
+        
+      cout << "Enter a positive integer and I will tell you its square" << endl;
+      
+      cout << "Enter 0 to quit" << endl;
+      
+        cin >> input;
+        
+        if (input == 0){
+            break;
+            
+        }
+        else {
+            cout << input << " becomes " << input * input << " when squared." << endl;
+        }
+    }
+  
+
+    return 0;
+}
